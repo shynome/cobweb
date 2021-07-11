723217cc681a2cfc1f6d336975f549979d96ce53
shynome <shynome@gmail.com>
2021-07-11 20:32:06 +0800

    api 添加 gzip 减小 qrcode 的大小

--- api/api.go
+++ api/api.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/shynome/cobweb/api/v2ray"
 )
 
 func Register(g *echo.Group) {
+	g.Use(middleware.Gzip())
 	v2ray.Register(g.Group("/v2ray"))
 }
